3536a0de0d034c13db6c6886ab3595cc66a3574a
Mattie Nickson <mnickson@sidingsmedia.com>
2025-05-04 12:35:10 +0100

    General repo setup
    Signed-off-by: Mattie Nickson <mnickson@sidingsmedia.com>

---
+++ go.mod
@@ -0,0 +1,6 @@
+// SPDX-FileCopyrightText: 2025 2025 Sidings Media <contact@sidingsmedia.com>
+// SPDX-License-Identifier: MIT
+
+module github.com/SidingsMedia/icmp-exporter
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 2025 Sidings Media <contact@sidingsmedia.com>
+// SPDX-License-Identifier: MIT
+
+package main
+
+import "log/slog";
+
+func main() {
+    slog.Info("Hello world");
+}
